312a6450583c8ef7e9fab63473b7bf6e52035add
Roman <roma564888@yandex.ru>
2021-10-05 12:45:11 +0300

    Added probability and delta

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function travelTime(crawlPath, adjacencyMatrix) {
     let sum = 0;
     for (let i = 0; i < crawlPath.length - 1; i++) {
@@ -10,7 +12,7 @@ function travelTime(crawlPath, adjacencyMatrix) {
 
 
 function shuffleArray(array) {
-    res = array;
+    let res = array;
     for (let i = 0; i < array.length * 3; i ++) {
         swapTwoRandom(res);
     }
@@ -18,11 +20,12 @@ function shuffleArray(array) {
 }
 
 function swapTwoRandom(crawlPath) {
-    res = crawlPath
-    x = parseInt(Math.random() * res.length);
-    do {
-        y = parseInt(Math.random() * res.length);
-    } while (x == y);
+    let res = crawlPath;
+    let x = parseInt(Math.random() * res.length);
+    let y = parseInt(Math.random() * res.length);
+    while (x == y) {
+        let y = parseInt(Math.random() * res.length);
+    }
     //console.log(x + " " + y);
     let temp = res[x];
     res[x] = res[y];
@@ -30,9 +33,12 @@ function swapTwoRandom(crawlPath) {
     //console.log(crawlPath);
 }
 
+function probability(deltaLengthPath, temperature) {
+    return 100 * Math.exp(-deltaLengthPath / temperature);
+}
 
 
-//задание весов рёбер
+
 let adjacencyMatrix = [[0, 3, 4, 1, 2, 1],
                        [3, 0, 1, 3, 4, 1],
                        [4, 1, 0, 1, 2, 1],
@@ -46,29 +52,58 @@ console.log(adjacencyMatrix);
 let crawlPath = [];
 crawlPath[0] = [];
 
+//длина пути
+let lengthPath = [];
+
 for (let i = 0; i < adjacencyMatrix.length; i++) {
     crawlPath[0][i] = i;
 }
 //console.log(crawlPath);
 //console.log(travelTime(crawlPath[0], adjacencyMatrix));
-let min = 9999999;
-let minPath = 0;
+//let min = 9999999;
+//let minPath = 0;
 //стартовый путь
 crawlPath[0] = shuffleArray(crawlPath[0]);
+lengthPath[0] = travelTime(crawlPath[0], adjacencyMatrix);
 console.log(crawlPath[0]);
-console.log(travelTime(crawlPath[0], adjacencyMatrix));
+console.log(lengthPath[0]);
 
-for (let i = 1; i < 10; i++) {
+//начальная температура
+let temperature = 100;
+
+//коэффициент снижения температуры
+let alpha = 0.95;
+
+//разница длин путей
+let deltaLengthPath = [];
+
+//вероятность для выбора пути
+//let probability;
+
+
+
+for (let i = 1; i < 15; i++) {
+    //новый путь обхода
     crawlPath[i] = shuffleArray(crawlPath[i - 1]);
+    //длина нового пути обхода
+    lengthPath[i] = travelTime(crawlPath[i], adjacencyMatrix);
+    //дельта путей обхода
+    deltaLengthPath[i] = lengthPath[i] - lengthPath[i - 1];
+
     console.log("Путь на итерации " + i + ": " + crawlPath[i]);
-    console.log("Длина пути на итерации " + i + ": " + travelTime(crawlPath[i], adjacencyMatrix));
-    if (travelTime(crawlPath[i], adjacencyMatrix) < min) {
-        min = travelTime(crawlPath[i], adjacencyMatrix);
-        minPath = i;
-    }
+    console.log("Длина пути на итерации " + i + ": " + lengthPath[i]);
+
+
+    
+    //if (travelTime(crawlPath[i], adjacencyMatrix) < min) {
+       // min = lengthPath[i];
+       // minPath = i;
+    //}
+    //снижаем температуру
+    temperature = temperature * alpha;
 }
 
-console.log("Кротчайший путь - №" + minPath + ", который имеет длину " + min);
+//console.log("Кротчайший путь " + crawlPath[minPath] + ", который имеет длину " + min);
 
 //let L = [];//массив длин путей
 
