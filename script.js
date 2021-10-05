05fb687d6f5298cecb61fc15d1d75adabde5193f
Roman <roma564888@yandex.ru>
2021-10-05 14:16:20 +0300

    Random int problem solved

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-"use strict";
+//"use strict";
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
 function travelTime(crawlPath, adjacencyMatrix) {
     let sum = 0;
@@ -10,34 +14,35 @@ function travelTime(crawlPath, adjacencyMatrix) {
     //console.log(sum);
 }
 
+function swapTwoRandom(crawlPath) {
+    let x = getRandomInt(crawlPath.length);
+    let y = getRandomInt(crawlPath.length);
+    while (x == y) {
+        console.log("Числа получились равные!");
+        y = getRandomInt(crawlPath.length);
+    }
+    console.log(x + " " + y);
+    let temp = crawlPath[x];
+    crawlPath[x] = crawlPath[y];
+    crawlPath[y] = temp;
+    //console.log(crawlPath);
+}
 
 function shuffleArray(array) {
     let res = array;
-    for (let i = 0; i < array.length * 3; i ++) {
+    //console.log(array.length);
+    for (let i = 0; i < array.length * 3; i++) {
         swapTwoRandom(res);
+        console.log("Итерация свапа " + i);
     }
     return res;
 }
 
-function swapTwoRandom(crawlPath) {
-    let res = crawlPath;
-    let x = parseInt(Math.random() * res.length);
-    let y = parseInt(Math.random() * res.length);
-    while (x == y) {
-        let y = parseInt(Math.random() * res.length);
-    }
-    //console.log(x + " " + y);
-    let temp = res[x];
-    res[x] = res[y];
-    res[y] = temp;
-    //console.log(crawlPath);
-}
+
 
 function probability(deltaLengthPath, temperature) {
-    return 100 * Math.exp(-deltaLengthPath / temperature);
+    return (100 * Math.exp(-deltaLengthPath / temperature));
 }
-
-
 
 let adjacencyMatrix = [[0, 3, 4, 1, 2, 1],
                        [3, 0, 1, 3, 4, 1],
@@ -46,7 +51,9 @@ let adjacencyMatrix = [[0, 3, 4, 1, 2, 1],
                        [2, 4, 2, 1, 0, 2],
                        [1, 1, 1, 1, 2, 0]];
 
-console.log(adjacencyMatrix);    
+//console.log(adjacencyMatrix);    
+
+let tempPath = [];
 
 //задание пути обхода
 let crawlPath = [];
@@ -56,23 +63,24 @@ crawlPath[0] = [];
 let lengthPath = [];
 
 for (let i = 0; i < adjacencyMatrix.length; i++) {
-    crawlPath[0][i] = i;
+    tempPath[i] = i;
 }
-//console.log(crawlPath);
-//console.log(travelTime(crawlPath[0], adjacencyMatrix));
-//let min = 9999999;
-//let minPath = 0;
+
+
+
 //стартовый путь
-crawlPath[0] = shuffleArray(crawlPath[0]);
+crawlPath[0] = shuffleArray(tempPath);
 lengthPath[0] = travelTime(crawlPath[0], adjacencyMatrix);
+
 console.log(crawlPath[0]);
 console.log(lengthPath[0]);
+
 
 //начальная температура
 let temperature = 100;
 
 //коэффициент снижения температуры
-let alpha = 0.95;
+let alpha = 0.5;
 
 //разница длин путей
 let deltaLengthPath = [];
@@ -80,9 +88,13 @@ let deltaLengthPath = [];
 //вероятность для выбора пути
 //let probability;
 
+let P = 0;
 
+let i = 1;
 
-for (let i = 1; i < 15; i++) {
+console.log("Текущая температура: " + temperature);
+while (temperature >= 1) {
+    
     //новый путь обхода
     crawlPath[i] = shuffleArray(crawlPath[i - 1]);
     //длина нового пути обхода
@@ -92,20 +104,20 @@ for (let i = 1; i < 15; i++) {
 
     console.log("Путь на итерации " + i + ": " + crawlPath[i]);
     console.log("Длина пути на итерации " + i + ": " + lengthPath[i]);
+    //вычисление вероятности для выбора пути
+    P = probability(deltaLengthPath[i], temperature);
 
-
+    //если P больше случайного числа от 0 до 100, выбираем этот путь
+    if (P > Math.random * 100) {
+        i++;
+    }
     
-    //if (travelTime(crawlPath[i], adjacencyMatrix) < min) {
-       // min = lengthPath[i];
-       // minPath = i;
-    //}
     //снижаем температуру
     temperature = temperature * alpha;
+
 }
 
-//console.log("Кротчайший путь " + crawlPath[minPath] + ", который имеет длину " + min);
-
-//let L = [];//массив длин путей
+console.log("Итоговый путь: " + crawlPath[crawlPath.length - 1]);
 
 
 
