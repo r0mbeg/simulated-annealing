f9a1da300023713009f26f580fa6c270b2c57759
Roman <roma564888@yandex.ru>
2021-10-19 17:49:20 +0300

    Made the implementation with time

--- script.js
+++ script.js
@@ -4,65 +4,129 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function travelTime(crawlPath, adjacencyMatrix) {
-    let sum = 0;
-    for (let i = 0; i < crawlPath.length - 1; i++) {
-        sum = sum + adjacencyMatrix[crawlPath[i]][crawlPath[i + 1]];
+function travelTime(crawlPath) {
+    let min = timeTable[0][crawlPath[0]];
+    let max = timeTable[0][crawlPath[0]];
+
+    for (let i = 0; i < crawlPath.length; i++) {
+        if (timeTable[i][crawlPath[i]] > max) {
+            max = timeTable[i][crawlPath[i]];
+        }
+        if (timeTable[i][crawlPath[i]] < min) {
+            min = timeTable[i][crawlPath[i]];
+        }
     }
-    return sum;
+
+    //console.log("min = " + min + ", max = " + max);
+
+    return (max - min);
 }
 
-function swapTwoRandom(crawlPath) {
+function changeOneRandom(crawlPath) {
     let tempCrawlPath = crawlPath.slice();
-    let x = getRandomInt(crawlPath.length);
-    let y = getRandomInt(crawlPath.length);
-    while (x == y) {
-        y = getRandomInt(crawlPath.length);
+    //номер врача, у которого будем менять время
+    let x = getRandomInt(timeTable.length);
+    
+    
+    //количество миллисекунд в минуте
+    let msMinutes = 1000 * 60;
+    let diff = 0;
+    //сформируем массив номерков, которые находятся не ближе 30 минут к остальным номеркам
+    //в последовательности посещения
+    let avaluable = [];
+    let flag = true;
+    //пробежим в цикле все номерки врача №x и проверим, какие из них подходят
+    for (let i = 0; i < timeTable[x].length; i++) {
+        flag = true;
+        for (let j = 0; j < crawlPath.length; j++) {
+            diff = (Math.abs(Math.floor(  (timeTable[x][i] - timeTable[j][crawlPath[j]]))) / msMinutes);
+            if (diff < 30) {
+                flag = false;
+                //console.log("Время " + timeTable[x][i] + " не подходит!");
+                break;
+            }
+        }
+        if (flag == true) {
+            avaluable.push(i);
+            //console.log("Время " + timeTable[x][i] + " подходит!");
+        }
     }
-    tempCrawlPath[x] = crawlPath[y];
-    tempCrawlPath[y] = crawlPath[x];
+
+    //если таких номерков нет, то запишем в массив тот номерок, который уже был выбран
+    if (avaluable.length == 0) {
+        avaluable.push(x);
+    }
+    //console.log("Список доступных номерков" + avaluable);
+    
+    
+    //среди свободных номерков выберем случайным образом один
+    let y = getRandomInt(avaluable.length);
+
+  //  console.log("У врача №" + x + " время изменено с " + crawlPath[x] + " : " 
+    //+ timeTable[x][crawlPath[x]] + " на " + avaluable[y] + " : " + timeTable[x][avaluable[y]]);
+    tempCrawlPath[x] = avaluable[y];
+
+
+    
     return tempCrawlPath;
+
+    
+
 }
 
-function shuffleArray(array) {
-    let res = array.slice();
-    for (let i = 0; i < array.length * 3; i++) {
-        res = swapTwoRandom(res);
-    }
-    return res;
+function shufflePath(array) {
+    
 }
 
 function probability(deltaLengthPath, temperature) {
-    return (100 * Math.exp(- (deltaLengthPath / temperature)));
+    return (100 * Math.exp(- (deltaLengthPath / (10000000 * temperature))));
 }
 
-let adjacencyMatrix = [[0, 3, 4, 1, 2, 1],
-                       [3, 0, 1, 3, 4, 1],
-                       [4, 1, 0, 1, 2, 1],
-                       [1, 3, 1, 0, 1, 1],
-                       [2, 4, 2, 1, 0, 2],
-                       [1, 1, 1, 1, 2, 0]];
+//задание расписания врачей
+//массив из 5 врачей
+let timeTable = new Array(5);
+for (let i = 0; i < timeTable.length; i ++) {
+    //у каждого врача 40 номерков с 8 до 17:45
+    timeTable[i] = new Array(40);
+    for (let j = 0; j < timeTable[i].length; j++) {
+        timeTable[i][j] = new Date(2021, 9, 18, 8, j * 15);
+    }   
+}
 
-///////////////////////////////
+/*
+for (let i = 0; i < timeTable.length; i ++) {
+    console.log("Расписание врача № " + i);
+    for (let j = 0; j < timeTable[i].length; j++) {
+        console.log("   " + j + ": " + timeTable[i][j]);
+    }
+}*/
 
 
 
-
-//////////////////////////////
-                       
-
-                       
-//задание пути обхода
+//последовательность обхода врачей
 let crawlPath = [];
-crawlPath[0] = [];
+crawlPath[0] = [0, 2, 4, 6, 8];
+let pathTime = [];
+pathTime[0] = travelTime(crawlPath[0]);
 
-//задание начального пути обхода
-for (let i = 0; i < adjacencyMatrix.length; i++) {
-    crawlPath[0][i] = i;
-}
-let lengthPath = [];
-lengthPath[0] = travelTime(crawlPath[0], adjacencyMatrix);
-console.log("Стартовый путь: " + crawlPath[0] + " имеет длину " + lengthPath[0] + "\n\n");
+let time = new Date(pathTime[0]);
+console.log("Последовательность обхода " + crawlPath[0] + " займёт время " + (time.getHours() - 3) + ":" + time.getMinutes());
+
+/*
+console.log("Исходная последовательность:");
+console.log("Врач №0 " + timeTable[0][crawlPath[0][0]]);
+console.log("Врач №1 " + timeTable[0][crawlPath[0][1]]);
+console.log("Врач №2 " + timeTable[0][crawlPath[0][2]]);
+console.log("Врач №3 " + timeTable[0][crawlPath[0][3]]);
+console.log("Врач №4 " + timeTable[0][crawlPath[0][4]]);
+*/
+
+//crawlPath[1] = changeOneRandom(crawlPath[0]);
+//console.log("\n");
+//pathTime[1] = travelTime(crawlPath[1]);
+//time = new Date(pathTime[1]);
+
+//console.log("Последовательность обхода " + crawlPath[1] + " займёт время " + (time.getHours() - 3) + ":" + time.getMinutes());
 
 //начальная температура
 let temperature = 100;
@@ -74,7 +138,7 @@ let alpha = 0.999;
 let deltaLengthPath = 0;
 
 //вероятность для выбора пути
-let P = 0;
+let p = 0;
 
 let i = 1;
 
@@ -84,44 +148,40 @@ let testP = Math.random() * 100;
 let res = 0;
 
 while (temperature >= 0.01) {
-    //новый путь обхода - меняему два случайных элемента
-    crawlPath[i] = swapTwoRandom(crawlPath[i - 1]);
-    //длина нового пути обхода
-    lengthPath[i] = travelTime(crawlPath[i], adjacencyMatrix);
+    crawlPath[i] = changeOneRandom(crawlPath[i - 1]);
+    pathTime[i] = travelTime(crawlPath[i]);
 
-
+    deltaPathTime = (pathTime[i] - pathTime[i - 1]);
     
-    //дельта путей обхода
-    deltaLengthPath = lengthPath[i] - lengthPath[i - 1];
-    
-    console.log("Путь на итерации " + i + ": " + crawlPath[i] + " имеет длину " + lengthPath[i] + " дельта = " + deltaLengthPath);
-    //если путь получился короче - переходим к следующей итерации
-    if (deltaLengthPath <= 0) {
-        console.log("Путь на итерации " + i +" удачный \n\n");
+    if (deltaPathTime <= 0) {
+        console.log("Путь на итерации " + i + " удачный \n\n");
         res = i;
         i++;
-    } else {//если путь получился длиннее - вычисляем P*
-        //вычисление "вероятности"      для выбора пути
-        P = probability(deltaLengthPath, temperature);
-        //если P больше случайного числа от 0 до 100, выбираем этот путь
+    } else {
+        p = probability(deltaPathTime, temperature);
+        console.log("p = exp(-" + deltaPathTime + "/(1000000 * " + temperature + ")) = " +p);
         testP = Math.random() * 100;
-        //console.log("Текущая температура: " + temperature + " Величина P* равна " + P + " Величина testP равна " + testP);
-        if (P > testP) {
-            console.log (P + " > " + testP + " - Путь на итерации " + i + " принят \n\n");
+
+        if (p > testP) {
+            console.log (p + " > " + testP + " - Путь на итерации " + i + " принят \n\n");
             res = i;
             i++;   
         } else {
-            
-            console.log (P + " <= " + testP + " - Путь на итерации " + i + " не принят \n\n");
+            console.log (p + " <= " + testP + " - Путь на итерации " + i + " не принят \n\n");
         }
+        
     }
-    temperature = temperature * alpha;
+    temperature *= alpha;
 }
 
-//настоящее решение - длина = 5
-
 console.log("Итоговый путь: " + crawlPath[res] + " имеет длину " + 
-lengthPath[res] + " достигнут на итерации " + (res) + "\n\n");
+(new Date(pathTime[res]).getHours() - 3) + ":" + (new Date(pathTime[res]).getMinutes()) +
+" достигнут на итерации " + (res) + "\n\n");
+
+
+
+
+
 
 
 
