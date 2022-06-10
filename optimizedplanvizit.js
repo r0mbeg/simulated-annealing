2d800d7042c52242cf7cb8bc8cd10bee72f13622
Roman <roma564888@yandex.ru>
2022-06-10 14:54:56 +0300

    Deleted spaces

--- optimizedplanvizit.js
+++ optimizedplanvizit.js
@@ -1,5 +1,3 @@
-//"use strict";
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -48,19 +46,15 @@ function travelLength(crawlPath, adjacencyMatrix) {
         return null;
     } else {
         let sum = 0;
-        
         let tempCrawlPath = crawlPath.slice();
-        
         //массив последователности обхода, формирующийся на основе crawlPath
         let crawlSequnce = [];
-    
         for (let i = 0; i < tempCrawlPath.length; i++) {
             crawlSequnce[i] =  minIndexArray(tempCrawlPath);
             //console.log("Минимум на итерации " + i + " равен элементу с индексом " + crawlSequnce[i]);
             //console.log(tempCrawlPath);
             tempCrawlPath[crawlSequnce[i]] = 9999;  
         }
-        
         for (let i = 0; i < crawlPath.length - 1; i++) {
             sum = sum + adjacencyMatrix[crawlSequnce[i]][crawlSequnce[i + 1]];
         }
@@ -88,7 +82,6 @@ function travelTime(crawlPath, timetable) {
     return (max - min - 10800000);
     }
 }
-
 //функция, меняющая у случайного врача время
 //при этом учитывается промежуток времени, 
 //в который пациент может посетить врачей
@@ -98,12 +91,9 @@ function changeOneRandom(crawlPath, startTime, endTime, timetable) {
         //console.log("Подобрать оптимальную последовательность невозможно!");
         return crawlPath;
     } else {
-     
     let tempCrawlPath = crawlPath.slice();
-    
     //номер врача, у которого будем менять время
     let x = getRandomInt(timetable.length);
-    
     //количество миллисекунд в минуте
     let msMinutes = 1000 * 60;
     let diff = 0;
@@ -112,7 +102,6 @@ function changeOneRandom(crawlPath, startTime, endTime, timetable) {
     //(!!!)avaluable - это именно номерки, а не времена
     let avaluable = [];
     let flag = true;
-    
     //пробежим в цикле все номерки врача №x и проверим, какие из них подходят
     for (let i = 0; i < timetable[x].length; i++) {
         flag = true;
@@ -160,12 +149,8 @@ function changeOneRandom(crawlPath, startTime, endTime, timetable) {
         //console.log("Подобрать оптимальную последовательность невозможно!");
         return null;
     }
+    }
 }
-}
-
-
-
-
 
 function shufflePath(array, startTime, endTime, timetable) {
       
@@ -193,8 +178,8 @@ function clearOptimizedPlanVizit() {
 }
 
 function selectCheckedDoctorsInMatrix(matrix, checkboxes) {
+    console.log(checkboxes);
     let resMatrix = matrix.slice();
-    //удаление строк
     for (let i = matrix.length - 1; i >= 0; i --) {
         if (!checkboxes[i].checked) {
             resMatrix.splice(i, 1);
@@ -203,6 +188,7 @@ function selectCheckedDoctorsInMatrix(matrix, checkboxes) {
     //удаление столбцов
     for (let i = resMatrix.length - 1; i >= 0; i --) {
         for (let j = resMatrix[0].length - 1; j >= 0; j --) {
+            
             if (!checkboxes[j].checked) {
                 resMatrix[i].splice(j, 1);
             }
@@ -211,52 +197,20 @@ function selectCheckedDoctorsInMatrix(matrix, checkboxes) {
     return resMatrix;
 }
 
-
-
-/*function getAdjacencyMatrix() {
-    let originalAdjacencyMatrix = [[0, 3, 4, 1, 1, 1, 5],
-                                   [3, 0, 1, 3, 1, 2, 4], 
-                                   [4, 1, 0, 1, 1, 3, 3],
-                                   [1, 3, 1, 0, 1, 4, 2],
-                                   [1, 1, 1, 1, 0, 5, 1],
-                                   [1, 2, 3, 4, 5, 0, 1],
-                                   [5, 4, 3, 2, 1, 1, 0]];
-    sessionStorage.setItem("originalAdjacencyMatrix", JSON.stringify(originalAdjacencyMatrix));
-    return originalAdjacencyMatrix;
-}*/
-
 var const_script = document.createElement("script");
     const_script.src="/js/optimizedplanvizit_const.js";
     document.head.appendChild(const_script);
     var originalAdjacencyMatrix = [];
-
     const_script.onload = function () {
         getAdjacencyMatrix();  
     }
 
 
 
-
-
-    var div_chart = document.createElement("div");
-            div_chart.setAttribute("id", "div_chart");
-            div_chart.style.width = "100px";
-            div_chart.style.height = "100px";
-            div_chart.style.background = "gray";
-            div_chart.style.color = "white";
-            div_chart.innerHTML = "Hello";
-
-
-
-
-
-
-function getOptimizedPlanVizit() {
     
+function getOptimizedPlanVizit() {
     originalAdjacencyMatrix = JSON.parse(sessionStorage.getItem("originalAdjacencyMatrix"));
-
     console.log(originalAdjacencyMatrix);
-
     sessionStorage.setItem('funcStart', 1);
     let allFieldsets = document.querySelectorAll('fieldset');
     let fieldsets = [];
@@ -292,25 +246,21 @@ function getOptimizedPlanVizit() {
                                        currTime[2]);   
         }
     }   
-
     let doctors = [];
 	    for (let i = 0; i < fieldsets.length; i ++) {
             doctors[i] = fieldsets[i].querySelector('legend').innerText.trim();
     }
     //console.log(doctors);
-
     let checkboxes = [];
-    
     //считываем отмеченных чекбоксами врачей
     //и заполняем массивы checked_*:
-
     let checkedDoctorsNumbers = [];
     let checkedDoctors = [];
     let checkedTimetable = [];
     let checkedButtons = [];
-
     for (let i = 0; i < fieldsets.length; i ++) {
         checkboxes[i] = document.getElementById("USEMED" + (i + 1));
+        
         if (checkboxes[i].checked) {
             checkedDoctorsNumbers.push(i);
             checkedDoctors.push(doctors[i]);
@@ -318,38 +268,27 @@ function getOptimizedPlanVizit() {
             checkedButtons.push(buttons[i]);
         }
     }
-
-
-    
-
     let adjacencyMatrix = selectCheckedDoctorsInMatrix(originalAdjacencyMatrix, checkboxes);
-   
-    
-
     //считывание окна времени для составления опт. последовательности
     timeStartObj = document.getElementById("TIMEBEGINMULTIVIZIT");
     timeEndObj = document.getElementById("TIMEENDMULTIVIZIT");
     timeStartAttribute = timeStartObj.value.split(":");
     timeEndAttribute = timeEndObj.value.split(":");
-   
     startTime = new Date(Number(dateStr[2]),
                          Number(dateStr[1] - 1),
                          Number(dateStr[0]), 
                          timeStartAttribute[0], 
                          timeStartAttribute[1],
                          timeStartAttribute[2]);
-
     endTime = new Date(Number(dateStr[2]), 
                        Number(dateStr[1] - 1), 
                        Number(dateStr[0]), 
                        timeEndAttribute[0], 
                        timeEndAttribute[1], 
                        timeEndAttribute[2]);
-
     //если количество элементов в хранилище сессии равно количеству врачей
     //то окрашиваем номерки
     if (sessionStorage.length > adjacencyMatrix.length) {
-
         let selectedButtons = [];
         for (let i = 0; i < allFieldsets.length; i++) {
             if (allFieldsets[i].querySelector('legend')) {
@@ -361,23 +300,19 @@ function getOptimizedPlanVizit() {
                 }
             }
         }
-        
         let selectedDoctors = [];
-
-
         //если врач выбран, то из массива doctors добавляем его в массивы selectedDoctors?????
         for (let i = 0; i < selectedButtons.length; i++) {             
             let tempDoc = selectedButtons[i].innerHTML.split(" ");
             selectedDoctors[i] = tempDoc[0] + " " + tempDoc[1] + " " + tempDoc[2];     
         }
-        
         //doctors и selectedDoctors имеют разный формат!
         //если какой-то из врачей уже выбран, то его номерок выделяться не будет
-
-
         for (let i = 0; i < doctors.length; i ++) {
             let flag = false;
+            
             if (checkboxes[i].checked) {
+                
                 for (let j = 0; j < selectedDoctors.length; j++) {
                     if (doctors[i] == selectedDoctors[j]) { 
                         flag = true;
@@ -396,30 +331,23 @@ function getOptimizedPlanVizit() {
                 }
             }
         }
-    } 
+    } else {
     //если последовательность ещё не была составлена 
     //(т.е. sessionStorage пуста и нужно вычислять оптимальную последовательность)
-    
-    else {
     let attemptCounter = 0;
     let success = false;
-
     while (attemptCounter <= 5 && !success) {
     //последовательность обхода врачей
     let crawlPath = [];
-
     console.log("Finding the optimal sequence in the interval from " +
     displayTime(startTime) + " to " + displayTime(endTime));
-
     crawlPath[0] = [];
     for (let i = 0; i < adjacencyMatrix.length; i++) {
         crawlPath[0][i] = 3*i;
     }     
         crawlPath[0] = shufflePath(crawlPath[0], startTime, endTime, checkedTimetable);
-        
         //продолжительности посещений
-        let pathTime = [];
-              
+        let pathTime = [];     
         pathTime[0] = travelTime(crawlPath[0], checkedTimetable);
         //длины путей между врачами
         let pathLength = [];
@@ -430,38 +358,27 @@ function getOptimizedPlanVizit() {
         //может быть увеличен если пациент, например, маломобилен
         let difficultyFactor = 700000;
         difficulty[0] = pathTime[0] + pathLength[0] * difficultyFactor;
-        
         let time = new Date(pathTime[0]);
-        
         if (crawlPath[0] != null) {
             console.log("Initial traversal sequence " + crawlPath[0] +
             " takes time " + displayTime(time) +
             " and has length " + travelLength(crawlPath[0], adjacencyMatrix));
         }
-        
         //начальная температура
         let temperature = 100;
-    
         //коэффициент снижения температуры
         let alpha = 0.999;
-    
         //сложность пути - функция от времени и
         //расстояния между врачами
         //вероятность для выбора пути
         let p = 0;
-    
         let i = 1;
-    
         let testP = Math.random() * 100;
-    
         //номер итогового пути
         let res = 0;
-        
         //разница между сложностями
         let deltaDifficulty = 0;
-    
         while (temperature >= 0.01) {
-            
             crawlPath[i] = changeOneRandom(crawlPath[i - 1], startTime, endTime, checkedTimetable);
                 if (crawlPath[i] == null) {
                     //console.log("Подобрать оптимальную последовательность невозможно!");
@@ -471,9 +388,7 @@ function getOptimizedPlanVizit() {
                     pathTime[i] = travelTime(crawlPath[i], checkedTimetable);
                     pathLength[i] = travelLength(crawlPath[i], adjacencyMatrix);
                     difficulty[i] = pathTime[i] + pathLength[i] * difficultyFactor;
-    
                     deltaDifficulty = difficulty[i] - difficulty[i - 1];
-    
                     if (deltaDifficulty <= 0) {
                         //console.log("Путь на итерации " + i + " удачный \n\n");
                         res = i;
@@ -482,7 +397,6 @@ function getOptimizedPlanVizit() {
                         p = probability(deltaDifficulty, temperature);
                         //console.log("p = exp(-" + deltaPathTime + "/(10000000 * " + temperature + ")) = " +p);
                         testP = Math.random() * 100;
-        
                         if (p > testP) {
                             //console.log (p + " > " + testP + " - Путь на итерации " + i + " принят \n\n");
                             res = i;
@@ -490,13 +404,10 @@ function getOptimizedPlanVizit() {
                         } else {
                             //console.log (p + " <= " + testP + " - Путь на итерации " + i + " не принят \n\n");
                         } 
-                 
                 } 
-    
             } 
             temperature *= alpha;    
         }
-        
         
         if (travelLength(crawlPath[res], adjacencyMatrix) != null) {
             success = true;
@@ -504,20 +415,13 @@ function getOptimizedPlanVizit() {
             displayTime(startTime) + " to " + displayTime(endTime) + " - " + 
             crawlPath[res] + " takes " + displayTime(new Date(pathTime[res])) +
             ", achieved on iteration " + res + " and has length " + travelLength(crawlPath[res], adjacencyMatrix));
-    
             console.log("Crawl sequence:");
-            
-            
             docCrawlPath = crawlPath[res].slice();
-            
-
-
             let strRes = "";
             for (let i = 0; i < docCrawlPath.length; i++) {
                 console.log(checkedDoctors[i] + " - " + displayTime(checkedTimetable[i][crawlPath[res][i]]));
                 strRes += checkedDoctors[i] + " - " + displayTime(checkedTimetable[i][crawlPath[res][i]]) + "\n";
             }
-    
             //alert(strRes);
             //раскрасим все кнопки в цвет по умолчанию 
             for (let i = 0; i < buttons.length; i ++) {
@@ -525,12 +429,10 @@ function getOptimizedPlanVizit() {
                     buttons[i][j].style.background = "";
                 }
             }
-            
             //выделим цветом выбранные номерки
             for (let i = 0; i < adjacencyMatrix.length; i++) {
                     checkedButtons[i][crawlPath[res][i]].style.background = "red";  
             }    
-    
             for (let i = 0; i < originalAdjacencyMatrix.length; i++) {
                 for (let j = 0; j < checkedDoctorsNumbers.length; j++) {
                     if (i == checkedDoctorsNumbers[j]) {
@@ -539,16 +441,6 @@ function getOptimizedPlanVizit() {
                 }           
             }       
             console.log(sessionStorage);
-
-            
-            document.body.appendChild(div_chart);
-
-            div_chart.innerHTML = res + " : " + travelLength(crawlPath[res], adjacencyMatrix);
-
-            
-
-
-
         } else {
             console.log("It is impossible to choose the optimal sequence!");
             attemptCounter++;
@@ -557,7 +449,6 @@ function getOptimizedPlanVizit() {
             } else if (attemptCounter == 5) {
                 alert("It is impossible to choose the optimal sequence!");
             }
-
         }
     }
     }
